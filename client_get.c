e4f5074cd31878ed8bac22bbee298ded5b964e3f
AdrienL <aleung-c@e1r11p5.42.fr>
2015-05-04 17:13:33 +0200

    push fin de journee ftp

--- client_get.c
+++ client_get.c
@@ -6,24 +6,29 @@
 /*   By: aleung-c <aleung-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/29 15:35:48 by aleung-c          #+#    #+#             */
-/*   Updated: 2015/04/30 16:30:11 by aleung-c         ###   ########.fr       */
+/*   Updated: 2015/05/04 17:12:18 by aleung-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "serveur.h"
 
-void client_get(int sock, char *buf_serv) // a peu pres OK.
+void client_get(int sock, char *buf_serv) // BUG avc fichiers non textes...
 {
 	int ret_serv;
 	int size_to_get;
 	char *buf;
+	char *file_name;
+	int fd_newfile;
+	char *buf_file;
+	struct stat file_stat;
 
-	ft_putstr("hey\n");
 	if (!ft_strstr(buf_serv, "ERROR")) // check READ 1 si error argunment. sinon receive size OK
 	{
 		// receive size.
+		file_name = ft_strsplit(buf_serv, '=')[2];
 		size_to_get = ft_atoi(ft_strsplit(buf_serv, '=')[1]);
 		printf("size to get = %d\n", size_to_get);
+		printf("name = %s\n", file_name);
 		// ft_putchar('\n');
 		//free(buf_serv);
 
@@ -36,11 +41,26 @@ void client_get(int sock, char *buf_serv) // a peu pres OK.
 		write(sock, buf, ft_strlen(buf));
 
 		// receive FILE.
-		ret_serv = read(sock, buf_serv, size_to_get);
+		buf_file = ft_strnew(size_to_get);
+		ret_serv = read(sock, buf_file, size_to_get);
 		// read content.
-		buf_serv[ret_serv] = '\0';
-		// mettre le buf dans un fichier => OPEN O_CREATE.
-		ft_putstr(buf_serv);
+		buf_file[ret_serv] = '\0';
+		// mettre le buf dans un fichier => OPEN O_CREAT.
+		fd_newfile = open(file_name, O_WRONLY | O_CREAT | O_APPEND, S_IRWXU | S_IRGRP | S_IROTH );
+		write(fd_newfile, buf_file, size_to_get);
+		close(fd_newfile);
+		//ft_putstr(buf_file);
+		ft_putstr("SUCCESS - File received\n");
+		
+
+		stat(file_name, &file_stat);
+		printf("name = %s, size = %d\n", file_name, (int)file_stat.st_size);
+		if (file_stat.st_size == size_to_get)
+		{
+			ft_putstr("SUCCESS - File reception OK\n");
+		}
+		else
+			ft_putstr("ERROR - File size incorrect. File may be corrupted");
 		ft_putstr(" --- READ 2\n");
 
 		// puis, check si taille recue est la bonne. si oui, return.
--- serv_get.c
+++ serv_get.c
@@ -6,7 +6,7 @@
 /*   By: aleung-c <aleung-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/28 17:18:00 by aleung-c          #+#    #+#             */
-/*   Updated: 2015/04/30 16:18:34 by aleung-c         ###   ########.fr       */
+/*   Updated: 2015/05/04 17:05:33 by aleung-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,14 +76,22 @@ void go_get(int cs, char **input)
 			//ft_putstr(file);
 			if (file == MAP_FAILED)
 			{
-				ft_putnbr(errno); //f
+				ft_putnbr(errno); //
 				perror("\nmmap"); //
+
+				buf_send =  ft_strdup("ERROR - Not a valid file\n");
+				ft_putstr(KGRN);
+				ft_putstr("[Sent]: ");
+				ft_putstr(RESET);
+				printf("%s\n", buf_send);
+				write(cs, buf_send, ft_strlen(buf_send));
 				return;
 			}
 			else
 				printf("mmap : SUCCESS\n");
 			buf_send =  ft_strjoin("SUCCESS - AWAITING READY SIGNAL get: mapping file. size=", ft_itoa(file_stat.st_size));
-			buf_send = ft_strjoin(buf_send, "\n");
+			buf_send = ft_strjoin(buf_send, ", name=");
+			buf_send = ft_strjoin(buf_send, input[1]);
 			ft_putstr(KGRN);
 			ft_putstr("[Sent]: ");
 			ft_putstr(RESET);
@@ -97,10 +105,13 @@ void go_get(int cs, char **input)
 			ft_putstr("[Received]: ");
 			ft_putstr(RESET);
 			printf("%s\n", buf_client);
-
 			// send file
 			if (ft_strstr(buf_client, "READY"))
-				write(cs, file, ft_strlen(file));
+			{
+				ft_putstr("SUCCESS - READY received\nSending ...\n");
+				// ft_putstr(file);
+				send(cs, file, (int)file_stat.st_size, MSG_OOB);
+			}
 			else
 			{
 				ft_putstr(KMAG);
@@ -114,7 +125,7 @@ void go_get(int cs, char **input)
 				printf("%s\n", buf_send);
 				write(cs, buf_send, ft_strlen(buf_send));
 			}
-
+			ft_putstr("SERVEUR GET - END\n");
 			return;
 		}
 
