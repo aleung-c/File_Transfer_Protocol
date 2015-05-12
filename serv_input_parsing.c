a9300e539435d14eb1379f538ff2736a241dde79
AdrienL <aleung-c@e1r11p3.42.fr>
2015-05-12 12:11:52 +0200

    rendu final ft_p

--- serv_input_parsing.c
+++ serv_input_parsing.c
@@ -6,7 +6,7 @@
 /*   By: aleung-c <aleung-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/01 13:46:25 by aleung-c          #+#    #+#             */
-/*   Updated: 2015/05/11 14:24:30 by aleung-c         ###   ########.fr       */
+/*   Updated: 2015/05/12 12:08:59 by aleung-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,18 +49,18 @@ void	check_input(int cs, char *buf_client)
 	input = get_input(buf_client);
 	if (!(input) || !(input[0]))
 		write_cs(cs, "ERROR", 1);
-	else if (ft_strcmp(input[0], "ls") == 0)
+	else if (ft_strnstr(input[0], "ls", 6))
 		go_ls(cs, input);
-	else if (ft_strcmp(input[0], "cd") == 0)
+	else if (ft_strnstr(input[0], "cd", 6))
 		go_cd(cs, input);
-	else if (ft_strcmp(input[0], "get") == 0)
+	else if (ft_strnstr(input[0], "get", 6))
 		go_get(cs, input);
-	else if (ft_strcmp(input[0], "put") == 0)
+	else if (ft_strnstr(input[0], "put", 6))
 		go_put(cs, input);
-	else if (ft_strcmp(input[0], "pwd") == 0)
+	else if (ft_strnstr(input[0], "pwd", 6))
 		go_pwd(cs);
-	else if (ft_strcmp(input[0], "quit") == 0
-		|| ft_strcmp(input[0], "exit") == 0)
+	else if (ft_strnstr(input[0], "quit", 6)
+		|| ft_strnstr(input[0], "exit", 6))
 	{
 		write_cs(cs, "SUCCESS - Quiting server ...", 1);
 		close(cs);
